b185b86388e933df7aaa6a174cf2a490947aeab7
michaelguarino <mguarino46@gmail.com>
2021-12-20 13:30:38 -0500

    add github ci to setup-plural action for testing

--- index.js
+++ index.js
@@ -1,14 +1,18 @@
 const core = require('@actions/core');
 const exec = require('@actions/exec');
 const tc = require('@actions/tool-cache');
+const io = require('@actions/io');
+
+const fs = require('fs');
+const path = require('path');
+
 
 const PLRL_URL = "https://app.plural.sh/artifacts/plural/plural?platform=linux&arch=amd64"
-
+const VSN = '1.0' //
 async function run() {
   try {
-    core.info(`donwload URL:         ${PLRL_URL}`)
-    const path = await tc.downloadTool(PLRL_URL);
-    core.addPath(path)
+    await download();
+    core.info("installed plural")
     await setupConfig();
     await exec.exec("plural --help");
   } catch (error) {
@@ -16,9 +20,23 @@ async function run() {
   }
 }
 
+async function download() {
+  core.info(`donwload URL: ${PLRL_URL}`)
+  const p = await tc.downloadTool(PLRL_URL);
+  core.info(`downloaded plural to ${p}`)
+  cachedPath = await tc.cacheFile(p, 'plural', 'plural', VSN);
+  await exec.exec(`chmod +x ${cachedPath}/plural`)
+  core.addPath(cachedPath)
+}
+
 async function setupConfig() {
   let conf = core.getInput('config');
-  await io.mkdirP('path/to/make')
+  const homedir = process.env.HOME
+  await io.mkdirP(path.join(homedir, ".plural"))
+  await fs.writeFile(path.join(homedir, ".plural", "config.yml"), conf, 'utf8', (err) => {
+    if (err) throw err
+    core.info('wrote config file')
+  })
 }
 
 run();
